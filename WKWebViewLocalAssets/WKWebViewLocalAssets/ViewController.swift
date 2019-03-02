72804c2c2857018bf4108a41544693b3f5305e1c
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-02 20:29:49 +0300

    Add local html assets and implement method loadWebViewContent.

--- WKWebViewLocalAssets/WKWebViewLocalAssets/ViewController.swift
+++ WKWebViewLocalAssets/WKWebViewLocalAssets/ViewController.swift
@@ -7,16 +7,49 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var webView: WebViewWrapper!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView.navigationDelegate = self
+        
+        loadWebViewContent()
     }
-
+    
+    /// Load local html resource as File or as String
+    func loadWebViewContent(_ file: String = "index", asFile: Bool = true) {
+        
+        guard let filePath = Bundle.main.path(forResource: file, ofType: "html",
+                                              inDirectory:  "LocalWebAssets") else {
+            print("Unable to load local html file: \(file)")
+            return
+        }
+        
+        if asFile {
+            // load local file
+            let filePathURL = URL.init(fileURLWithPath: filePath)
+            let fileDirectoryURL = filePathURL.deletingLastPathComponent()
+            webView.loadFileURL(filePathURL, allowingReadAccessTo: fileDirectoryURL)
+        } else {
+            do {
+                // load html string. baseURL is required for local files to load correctly
+                let html = try String(contentsOfFile: filePath, encoding: .utf8)
+                webView.loadHTMLString(html, baseURL: Bundle.main.resourceURL?.appendingPathComponent("LocalWebAssets"))
+            } catch {
+                print("Unable to load local html resource as string")
+            }
+        }
+    }
 
 }
 
+extension ViewController : WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Delegate method didFinish navigation:");
+    }
+}
