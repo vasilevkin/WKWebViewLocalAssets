94015590a118fa0550297bc5032588f02570b645
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-02 11:10:25 +0300

    Add new project iOS Single View App.

---
+++ WKWebViewLocalAssets/WKWebViewLocalAssets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKWebViewLocalAssets
+//
+//  Created by Sergey Vasilevkin on 02/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
